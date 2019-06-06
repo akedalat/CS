cc21a73e9691c1fe42f7ed5dc96b83e05f260812
Kian Edalat <akedalat@gmail.com>
2019-06-06 17:12:25 +0100

    sumUpTo(n)

---
+++ sumUpTo.js
@@ -0,0 +1,10 @@
+function sumUpTo(n){
+    if (n > 1){
+        //Remember in Recursion (function call itself)
+        //it has to call the function uo till the end (base case)
+        //and then returns back up to the first call/function
+        return sumUpTo(n - 1) + n
+    } else {
+        return 1	
+        }
+    }
