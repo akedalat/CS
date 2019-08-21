c7417af837c524f2565609f42f6fddf6d8059566
Kian <kian@Kians-MacBook-Pro.local>
2019-08-21 15:32:17 +0100

    Frequency Counter

--- udemyPractices.js
+++ udemyPractices.js
@@ -6,3 +6,37 @@ function areThereDuplicates() {
  } 
  return Object.values(obj).includes(2) ? true : false
 } 
+
+// Frequency Counter
+// Check if two numbers have same numbers in them
+function sameFrequency(int1, int2){
+    let array1 = Array.from(String(int1), Number);
+    let array2 = Array.from(String(int2), Number);
+    let obj1 = {};
+    let obj2 = {};
+      if(array1.length !== array2.length){
+          return false;
+      } else
+      for(let i = 0; i < array1.length; i++){
+          if(obj1[array1[i]]){
+            obj1[array1[i]] += 1;
+          } else { 
+            obj1[array1[i]] = 1;
+          }
+      }
+      for(let i = 0; i < array2.length; i++){
+          if(obj2[array2[i]]){
+            obj2[array2[i]] += 1;
+          } else { 
+            obj2[array2[i]] = 1;
+          }
+      }
+      for(let key in obj1){
+          if(!(key in obj2)){
+              return false;
+          } if (obj1[key] != obj2[key]){
+              return false;
+          }
+      }
+      return true;
+    }
