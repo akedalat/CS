3626e26ba6f4a6896a4f97fceecf2a09028706e7
Kian <kian@kians-mbp.home>
2019-08-26 10:09:41 +0100

    Average Pair

--- arrays.js
+++ arrays.js
@@ -55,3 +55,20 @@ function sameFrequency(int1, int2){
       }
       return true;
     }
+
+    // Average Pair
+    // Check if a average pair equals targeted value 
+    function averagePair(array, avgNum){
+      let first = 0;
+      let last = array.length - 1;
+       while (first < last){
+          let tempAvg = ((array[first] + array[last]) / 2);
+          if(tempAvg === avgNum){return true;}
+          if (tempAvg > avgNum) {
+          last--;
+          } else {
+          first++;
+          }
+         }
+     return false;
+     }
