a62c6c5a48fff6d5ddc1a93d3950a8a294c1fa80
Kian Edalat <kian@kians-mbp.lan>
2019-11-04 18:47:30 +0000

    bubbleSort implemented

--- sorting_algorithms.js
+++ sorting_algorithms.js
@@ -21,3 +21,33 @@ function insertionSort(array) {
   }
   return sorted;
 }
+
+// Bubble Sort
+// It checks for the largest number and put in the end of array
+function bubbleSort(array) {
+  // We use noSwap to avoid unnecessary iteration
+  let noSwap;
+  // The last number will be the largest in the 1st loop,
+  // So we don't need to check the last number again
+  for (let i = array.length; i > 0; i--) {
+    noSwap = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+        noSwap = false;
+      }
+      if (noSwap) {
+        console.log(array);
+        return array;
+      }
+    }
+  }
+  console.log(array);
+  return array;
+}
+bubbleSort([6, 1, 2, 3, 4, 5]);
+
+//Selection Sort
+// It checks for smallest number and put it in front of array
