4b7e1a636544dee0cd12a317cbe53a378b9465e5
Kian Edalat <kian@kians-mbp.lan>
2019-11-04 19:32:36 +0000

    selectionSort

--- sorting_algorithms.js
+++ sorting_algorithms.js
@@ -47,7 +47,28 @@ function bubbleSort(array) {
   console.log(array);
   return array;
 }
-bubbleSort([6, 1, 2, 3, 4, 5]);
+//bubbleSort([6, 1, 2, 3, 4, 5]);
 
 //Selection Sort
 // It checks for smallest number and put it in front of array
+function selectionSort(array) {
+  let lowest;
+  for (let i = 0; i < array.length; i++) {
+    lowest = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[lowest]) {
+        lowest = j;
+      }
+    }
+    //Using if to avoid unnecessary swap
+    if (i !== lowest) {
+      let temp = array[i];
+      array[i] = array[lowest];
+      array[lowest] = temp;
+    }
+  }
+  console.log(array);
+  return array;
+}
+
+selectionSort([3, 4, 7, 1, 8]);
