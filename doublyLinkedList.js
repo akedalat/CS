d578806b02e99d8722e20acc4e13fd783a0c6cfb
Kian Edalat <akedalat@gmail.com>
2020-01-16 19:53:03 +0000

    added pop shift and unshift

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -25,6 +25,56 @@ class DoublyLinkedList {
         this.length++
         return this
     }
+
+    pop(){
+       if(this.length === 0){
+           return undefined
+       }
+       let oldTail = this.tail 
+       if(this.length === 1){
+           this.tail = null
+           this.head = null
+           this.length--
+           return oldTail
+       }
+       
+       this.tail = this.tail.prev
+       this.tail.prev.next = null
+       this.length--
+       return oldTail
+    }
+
+    shift(){
+        if(this.length === 0){
+            return undefined
+        }
+        let oldHead = this.head
+        if(this.length === 1){
+            this.tail = null
+            this.head = null
+            this.length --
+            return oldHead
+        }
+        
+        this.head = oldHead.next
+        this.head.prev = null
+        oldHead.next = null
+        this.length--
+        return oldHead
+    }
+    unshift(val){
+        let newNode = new Node(val)
+        if(this.length === 0){
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.head.prev = newNode
+            newNode.next = this.head
+            this.head = newNode
+        }
+        this.length++
+        return this
+    }
 }
 
 
