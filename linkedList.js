f6761d88dafb26d6df74a2093ec8acc31290d92d
Kian Edalat <akedalat@gmail.com>
2020-01-19 22:30:28 +0000

    added get set and insert

--- linkedList.js
+++ linkedList.js
@@ -1,25 +1,25 @@
 // First element is value and second element is address of the next node
-const firstNode = ['cameron', 123]
-const secondNode = ['sloane', 132]
-const thirdNode = ['ferris', null]
+const firstNode = ['cameron', 123];
+const secondNode = ['sloane', 132];
+const thirdNode = ['ferris', null];
 
-const collection = { 0: firstNode, 123: secondNode, 132: thirdNode }
-const head = collection[0]
+const collection = {0: firstNode, 123: secondNode, 132: thirdNode};
+const head = collection[0];
 
-function next (node) {
-  const nextAddress = node[1]
+function next(node) {
+  const nextAddress = node[1];
   // retrieve the address of the next element
-  return collection[nextAddress]
+  return collection[nextAddress];
 }
 
 // Find the next node by index
-function indexAt (head, index) {
-  let node = head
+function indexAt(head, index) {
+  let node = head;
   for (let i = 0; i < index; i++) {
-    node = next(node)
+    node = next(node);
   }
 
-  return node
+  return node;
 }
 
 // indexAt(firstNode, 2) would give:
